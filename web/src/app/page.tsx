f69e4b7b0df17871a789daa2ec02a6308103f03f
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-08-09 03:36:23 +0000

    🪄 criando projeto com tailwind

---
+++ web/src/app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div className="p-8 bg-slate-900 text-slate-100 h-screen">
+      <h1 className="font-bold text-5xl text-rocket">
+        Hello Tailwind
+      </h1>
+
+      <p>Text</p>
+    </div>
+  )
+}
---
+++ web/tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/**/*.tsx',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        rocket: '#8257e6',
+      }
+    },
+  },
+  plugins: [],
+}
+export default config
