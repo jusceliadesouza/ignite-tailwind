c1447ac22173f70934e660f2d323dab9dd12134f
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-08-09 15:08:25 +0000

    🧱 estrutura do layout

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,20 +1,6 @@
 
 export default function Home() {
   return (
-    <div className="p-8 bg-slate-100 text-slate-900 dark:bg-slate-900 dark:text-slate-100 h-screen flex flex-col text-center">
-      <div className="max-w-app">
-        <h1 className="font-bold text-3xl sm:text-4xl lg:text-5xl flex items-center gap-3">
-          Rapidly build modern websites without ever leaving your HTML
-        </h1>
-
-        <p className="mt-4 dark:text-slate-400">
-          A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup.
-        </p>
-
-        <button className="bg-sky-500 text-white dark:bg-sky-400 dark:text-slate-900 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">
-          Sign in
-        </button>
-      </div>
-    </div>
+        <h1>Hello World</h1>
   )
 }
