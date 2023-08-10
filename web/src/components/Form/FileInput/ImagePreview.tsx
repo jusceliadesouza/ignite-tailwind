890206b25e528c92190f592459bc8cb92a020144
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-08-10 17:28:12 -0300

    🛣️ context e preview da imagem

--- web/src/components/Form/FileInput/ImagePreview.tsx
+++ web/src/components/Form/FileInput/ImagePreview.tsx
@@ -1,9 +1,34 @@
+'use client'
+
 import { LuUser } from 'react-icons/lu'
+import { UseFileInput } from './Root'
+import { useMemo } from 'react'
 
 export function ImagePreview() {
-  return (
-    <div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
-      <LuUser className="h-8 w-8 text-violet-500 " />
-    </div>
-  )
+  const { files } = UseFileInput()
+
+  const previewURL = useMemo(() => {
+    if (files.length === 0) {
+      return null
+    }
+
+    return URL.createObjectURL(files[0])
+  }, [files])
+
+  if (previewURL === null) {
+    return (
+      <div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
+        <LuUser className="h-8 w-8 text-violet-500 " />
+      </div>
+    )
+  } else {
+    return (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img
+        src={previewURL}
+        className="h-16 w-16 rounded-full object-cover"
+        alt=""
+      />
+    )
+  }
 }
