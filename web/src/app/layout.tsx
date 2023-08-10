9d9ce8909ab350d3571ff99a35799f6b94d72e8e
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-08-09 21:27:54 -0300

    🪄 logo e input de busca

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Sidebar } from '@/components/Sidebar'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -17,8 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className='min-h-screen grid grid-cols-app'>
-          <aside className='border-r border-zinc-200 px-5 py-8'>Sidebar</aside>
+      <div className='min-h-screen grid grid-cols-app'>
+          <Sidebar />
 
           <main className="px-4 pb-12 pt-8">
             {children}
