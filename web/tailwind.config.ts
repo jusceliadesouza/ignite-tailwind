079515d61a0e8a0fb17b341c6ecaf5da1dfacaa3
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-08-10 01:09:02 -0300

    🪄 campos do formulário

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -8,7 +8,8 @@ module.exports = {
     extend: {
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
-        profile: 'max-content 1fr min-content'
+        profile: 'max-content 1fr min-content',
+        form: 'minmax(75rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)'
       },
       maxWidth: {
         app: '700px',
